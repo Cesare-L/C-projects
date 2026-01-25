193cc88630d6b8e0764ffa37908fade18e317608
Cesare-L <111635700+Cesare-L@users.noreply.github.com>
2026-01-25 18:07:24 +0000

    Create Farenheit-to-Celcius.c

---
+++ Farenheit-to-Celcius.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+   int fahr, celcius;
+   int upper, lower, step;
+
+  lower = 0;
+  upper 300;
+
+  step = 20;
+
+ fahr = lower;
+  while(fahr <= upper) {
+    celcius = fahr - 32;
+    celcius = celcius / 1.8;
+    printf("%i // %i \n", celcius, fahr);
+    fahr = fahr + step;
+  }
+  
+  return 0;
+}
