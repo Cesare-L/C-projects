1094c312c64e59429f0bd14578a353ecf47ef991
Cesare-L <111635700+Cesare-L@users.noreply.github.com>
2026-01-25 23:22:11 +0000

    Update Farenheit-to-Celcius.c

--- Farenheit-to-Celcius.c
+++ Farenheit-to-Celcius.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#define lower 0
-#define upper 300
-#define step 20
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 int main(void) {
    float fahr, celcius;
 
- fahr = lower;
-  while(fahr <= upper) {
+ fahr = LOWER;
+  while(fahr <= UPPER) {
     celcius = fahr - 32;
     celcius = celcius / 1.8;
     printf("%.2f // %.2f \n", celcius, fahr);
-    fahr = fahr + step;
+    fahr = fahr + STEP;
   }
   
   return 0;
