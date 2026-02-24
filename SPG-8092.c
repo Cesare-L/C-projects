fc73dddfd7f2b604ab623a99d46f51ac44818e9b
Cesare-L <111635700+Cesare-L@users.noreply.github.com>
2026-02-24 23:24:19 +0100

    Create SPG-8092.c

---
+++ SPG-8092.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        printf("///////////////////////////////////////////////////////\n");
+        printf("\nThis program will generate a random 8092 bit string of text (PASSPHRASE)");
+        printf("\nUsage: %s <generate>\n", argv[0]);
+        printf("\n///////////////////////////////////////////////////////\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "generate") == 0 || strcmp(argv[1], "Generate") == 0) {
+        char passphrase[1350] = "";
+        const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!%@#$^&*()_+-=[]{}|;:,.<>?|~";
+        unsigned int charset_size = (unsigned int)strlen(charset);
+        for (int i = 0; i < 1349; i++) {
+            passphrase[i] = charset[arc4random_uniform(charset_size)];
+        }
+        passphrase[1349] = '\0';
+        printf("GENERATED PASSPHRASE: %s\n", passphrase);
+        return 0;
+    }
+    
+    for(int i = 0; i < argc; i++) {
+        printf(" %s", argv[i]);
+    }
+
+    printf("\n");
+
+}
